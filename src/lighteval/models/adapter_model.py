e46c51febfe81aba63590f6adbb7827dd0655956
Atsuki Yamaguchi <acp23ay@bessemer-login1.shef.ac.uk>
2024-05-03 20:57:46 +0100

    Remove unneessary lines

--- src/lighteval/models/adapter_model.py
+++ src/lighteval/models/adapter_model.py
@@ -47,40 +47,19 @@ class AdapterModel(BaseModel):
         torch_dtype = _get_dtype(config.dtype, self._config)
         config.model_parallel, max_memory, device_map = self.init_model_parallel(config.model_parallel)
         adapter_weights = config.pretrained
-
-        #merged_path = f"{adapter_weights}-adapter-applied"
-
-        if self.accelerator.is_local_main_process if self.accelerator is not None else nullcontext():
-            hlog(f"Loading model from {adapter_weights} and applying adapter to {config.base_model}")
-            base = AutoModelForCausalLM.from_pretrained(
-                config.base_model,  
-                low_cpu_mem_usage=True, 
-                max_memory=max_memory,
-                device_map=device_map,
-                torch_dtype=torch.float16 if torch_dtype == torch.float64 else torch_dtype,
-                trust_remote_code=config.trust_remote_code,
-                cache_dir=env_config.cache_dir,
-                quantization_config=config.quantization_config,
-                token=env_config.token,
-            )
-            model = PeftModelForCausalLM.from_pretrained(base, adapter_weights)
-            #model = model.merge_and_unload()
-            #hlog("Saving model with adapter applied")
-            #base.save_pretrained(merged_path)
-
-            return model
+        hlog(f"Loading model from {adapter_weights} and applying adapter to {config.base_model}")
+        base = AutoModelForCausalLM.from_pretrained(
+            config.base_model,  
+            low_cpu_mem_usage=True, 
+            max_memory=max_memory,
+            device_map=device_map,
+            torch_dtype=torch.float16 if torch_dtype == torch.float64 else torch_dtype,
+            trust_remote_code=config.trust_remote_code,
+            cache_dir=env_config.cache_dir,
+            quantization_config=config.quantization_config,
+            token=env_config.token,
+        )
+        model = PeftModelForCausalLM.from_pretrained(base, adapter_weights)
         
-        #hlog(f"Loading model from {merged_path}")
-        #model = AutoModelForCausalLM.from_pretrained(
-        #    merged_path,
-        #    max_memory=max_memory,
-        #    device_map=device_map,
-        #    torch_dtype=torch_dtype,
-        #    trust_remote_code=config.trust_remote_code,
-        #    cache_dir=env_config.cache_dir,
-        #    quantization_config=config.quantization_config,
-        #    token=env_config.token,
-        #)  
+        return model
         
-        else:
-            raise NotImplementedError
